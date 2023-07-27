8de258448dd0cf522a3efd43fbd52908ebfc2261
OJII3 <84656786+OJII3@users.noreply.github.com>
2023-07-27 18:34:07 +0900

    commit

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import * as _ from "./.wasm/wasm_exec.js";
 
+import { readAll } from "https://deno.land/std@0.187.0/streams/read_all.ts";
+
 const go = new window.Go();
 const f = await Deno.open("./.wasm/main.wasm");
-const bytes = await Deno.readAll(f);
+const bytes = await readAll(f);
 const inst = await WebAssembly.instantiate(bytes, go.importObject);
 go.run(inst.instance);
